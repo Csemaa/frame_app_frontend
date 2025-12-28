3b465e54ec580d9ae6790961b7387e0c93ff498c
Csemeczki, András <andras.csemeczki@gmail.com>
2025-12-28 21:48:15 +0100

    slightly better cards

--- src/components/authentication/UserCards.tsx
+++ src/components/authentication/UserCards.tsx
@@ -1,17 +1,55 @@
 import useUsers from "@/hooks/use-users"
+import { getAvatar } from "@/utils/get-avatar"
+import { Box, Card, Heading, Image, SkeletonCircle, SkeletonText, Text } from "@chakra-ui/react"
+
 
 const UserCards = () => {
   const { users, error, isPending } = useUsers()
+  const skeletonArray = [1, 2, 3, 4];
+  if (isPending) return (
+    <Box display={'flex'} alignItems={'center'} gap={4} my={5}>
+      {
+        skeletonArray.map(e =>
+          <Card.Root key={e} width={'20rem'}>
+            <Card.Body>
+              <Box display={'flex'} alignItems={'center'} gap={4}>
+                <SkeletonCircle size="70px" />
+                <SkeletonText noOfLines={1} />
+              </Box>
+            </Card.Body>
+          </Card.Root>
+        )
+      }
+    </Box>
+  )
 
-  if (isPending) return <p>Loading</p>
-  if (error) return <p>{error.message}</p>
+  if (error) return <Text color={'red'}>{error.message}</Text>
+
+  if (!users) {
+    return <Heading>No users added yet</Heading>
+  }
 
   return (
-    <div>
-      {users?.map(user => (
-        <p key={user.id}>{user.email}</p>
-      ))}
-    </div>
+    <Box p={3}>
+      <Heading fontSize={'2rem'} my={5}>Select a user</Heading>
+      <Box display={'flex'} alignItems={'center'} gap={4}>
+        {users.map(user => (
+          <Card.Root key={user.id} width={'20rem'}>
+            <Card.Body>
+              <Box display={'flex'} alignItems={'center'} gap={4}>
+                <Image
+                  src={(getAvatar(user.profile_picture))}
+                  alt="User avatar"
+                  boxSize="70px"
+                  borderRadius={'50%'}
+                />
+                <Card.Title>{user.nickname}</Card.Title>
+              </Box>
+            </Card.Body>
+          </Card.Root>
+        ))}
+      </Box>
+    </Box>
   )
 }
 
---
+++ src/utils/get-avatar.ts
@@ -0,0 +1,19 @@
+import avatar_1 from '../assets/avatars/avatar_1.png'
+import avatar_2 from '../assets/avatars/avatar_2.png'
+import avatar_3 from '../assets/avatars/avatar_3.png'
+import avatar_4 from '../assets/avatars/avatar_4.png'
+
+export const getAvatar = (index: string) => {
+  switch (index) {
+    case "1":
+        return avatar_1
+    case "2":
+        return avatar_2
+    case "3":
+        return avatar_3
+    case "4":
+        return avatar_4
+    default:
+        return avatar_1
+  }
+}
